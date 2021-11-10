b6374a98af048702dd9a4facab6c16a49ebbe806
Viktor Prokopenko <victor.prokopenko@gmail.com>
2021-11-10 22:55:27 +0200

    10 Nov 2021

--- Lesson1/Lsn1.js
+++ Lesson1/Lsn1.js
@@ -1,20 +1,19 @@
 let array=[' hello',' owu',' com',' ua']; //масив
 var num1=1; //глобальна змінна
 let num2=10;
-let numarray=[-999, 123, 3.14, 2.7, 16];
+let numarray=[-999, 123, 3.14, 2.7, 16];//масив
 let bul1=true;
 let bul2=false;
 console.log(array);
-console.log(num1);
-console.log(num2);
+console.log(num1); console.log(num2);
 console.log(numarray);
 console.log(bul1, bul2);
 alert(array);
 alert(num1);
 alert(num2);
-alert(numarray); alert(numarray[0]+" "+num2);
+alert(numarray); alert(numarray[2]+num2);
 alert(bul1+" "+bul2);
-document.write(array+" "); document.write(array[0]+array[1]+array[2]+'<br \\/>');
+document.write(array+" "); document.write(array[0]+array[1]+array[2]+array[3]+'<br \\/>');
 document.write(num1+num2+'<br \\/>');
 document.write(num1," ",num2,'<br \\/>');
 document.write(numarray+'<br \\/>');
