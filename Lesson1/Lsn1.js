124a05f2ef913e00ec52240bec5271d910e60add
viktorpro <victor.prokopenko@gmail.com>
2021-11-11 12:04:21 +0200

    11/11/2021

--- Lesson1/Lsn1.js
+++ Lesson1/Lsn1.js
@@ -1,9 +1,7 @@
 let array0=[' hello',' owu',' com',' ua']; //масив
-let num1=1; //
-let num2=10;
+let num1=1; let num2=10;
 let array1=[-999, 123, 3.14, 2.7, 16];//масив
-let bul1=true;
-let bul2=false;
+let bul1=true; let bul2=false;
 console.log(array0);
 console.log(num1); console.log(num2);
 console.log(array1);
@@ -13,7 +11,7 @@ alert(num1);
 alert(num2);
 alert(array1); //alert(array1[2]+num2); // як числа
 alert(bul1+" "+bul2);
-document.write(array0+" "); document.write(array0[0]+array0[1]+"."+array0[2]+"."+array0[3]+'<br \/>');
+document.write(array0+" "+'<br \/>'); document.write(array0[0]+array0[1]+"."+array0[2]+"."+array0[3]+'<br \/>');
 //document.write(num1+num2); //+'<br \/>'
 num1 = num1+" "; num2 = num2+" ";
 document.write(num1+num2+'<br \/>');
@@ -22,7 +20,28 @@ document.write(array1+'<br \/>'); // +'<br \/>'
 bul1 = bul1+" "; bul2 = bul2+" ";
 document.write(bul1, bul2);
 //Переприсвоїти кожній змінній з завдання значення на довільне.
-
+bul1=false;
+bul2=true;
+//Поміняємо масиви.
+array0=[2,0,2,1];
+array1=["-999", "123", "3.14", "2.7", "16"];
+//
+num1="One"; num2="Ten";
+console.log(" "+array0);
+console.log(num1+" "); console.log(" "+num2);
+console.log(array1);
+console.log(bul1, bul2);
+alert(array0);
+alert(num1); alert(num2);
+alert(array1); //alert(array1[2]+num2); // як числа
+alert(bul1+" "+bul2);
+array0 = array0+' ';
+document.write(array0+" "+'<br \/>');
+document.write(num1+num2+'<br \/>');
+document.write(array1+'<br \/>');
+bul1 = bul1+" "; bul2 = bul2+" ";
+document.write(bul1, bul2);
+//firstName, middleName, lastName person
 
 
 
