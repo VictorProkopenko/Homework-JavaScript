338cced1e3e70f46449be6e733f0c1d1c5ea84c4
viktorpro <victor.prokopenko@gmail.com>
2021-11-11 11:39:55 +0200

    11/11/2021

--- Lesson1/Lsn1.js
+++ Lesson1/Lsn1.js
@@ -1,23 +1,27 @@
-let array=[' hello',' owu',' com',' ua']; //масив
-var num1=1; //глобальна змінна
+let array0=[' hello',' owu',' com',' ua']; //масив
+let num1=1; //
 let num2=10;
-let numarray=[-999, 123, 3.14, 2.7, 16];//масив
+let array1=[-999, 123, 3.14, 2.7, 16];//масив
 let bul1=true;
 let bul2=false;
-console.log(array);
+console.log(array0);
 console.log(num1); console.log(num2);
-console.log(numarray);
+console.log(array1);
 console.log(bul1, bul2);
-alert(array);
+alert(array0);
 alert(num1);
 alert(num2);
-alert(numarray); alert(numarray[2]+num2);
+alert(array1); //alert(array1[2]+num2); // як числа
 alert(bul1+" "+bul2);
-document.write(array+" "); document.write(array[0]+array[1]+array[2]+array[3]+'<br \\/>');
-document.write(num1+num2+'<br \\/>');
-document.write(num1," ",num2,'<br \\/>');
-document.write(numarray+'<br \\/>');
-document.write(bul1, " ", bul2);
+document.write(array0+" "); document.write(array0[0]+array0[1]+"."+array0[2]+"."+array0[3]+'<br \/>');
+//document.write(num1+num2); //+'<br \/>'
+num1 = num1+" "; num2 = num2+" ";
+document.write(num1+num2+'<br \/>');
+array1 = array1+' ';
+document.write(array1+'<br \/>'); // +'<br \/>'
+bul1 = bul1+" "; bul2 = bul2+" ";
+document.write(bul1, bul2);
+//Переприсвоїти кожній змінній з завдання значення на довільне.
 
 
 
