1600e33f58203e8ec6d2c913926d027fdb50f4d7
viktorpro <victor.prokopenko@gmail.com>
2021-11-13 17:47:42 +0200

    13/11/2021

--- Lesson2/Lsn2.js
+++ Lesson2/Lsn2.js
@@ -1,30 +1,30 @@
 // 1.
 let time;
 time = Math.floor(Math.random() * 60); // згенеруємо число від 0 до 59
-if(time <= 15){
-    console.log(time+' хвилини - перша чверть години');
- } else if(time <= 30){
-    console.log(time+' хвилини - друга чверть години');
- }else if(time <= 45){
-    console.log(time+' хвилини - третя чверть години');
- } else if(time <= 59) {
-    console.log(time+' хвилини - четверта чверть години');
- }
+if (time <= 15) {
+    console.log(time + ' хвилини - перша чверть години');
+} else if (time <= 30) {
+    console.log(time + ' хвилини - друга чверть години');
+} else if (time <= 45) {
+    console.log(time + ' хвилини - третя чверть години');
+} else if (time <= 59) {
+    console.log(time + ' хвилини - четверта чверть години');
+}
 // 2.
 let day;
-day = (Math.floor(Math.random() * 31))+1; // згенеруємо число від 1 до 31
- if(day <= 11) {
-    console.log(day+' число - перша декада місяця');
- } else if(day <= 21) {
-    console.log(day+' число - друга декада місяця');
- } else if(day <= 31) {
-    console.log(day+' число - третя декада місяця');
- }
+day = (Math.floor(Math.random() * 31)) + 1; // згенеруємо число від 1 до 31
+if (day <= 11) {
+    console.log(day + ' число - перша декада місяця');
+} else if (day <= 21) {
+    console.log(day + ' число - друга декада місяця');
+} else if (day <= 31) {
+    console.log(day + ' число - третя декада місяця');
+}
 
 // 3. змінна test, якщо  не дорівнює true, виведіть 'Вірно', інакше виведіть 'Неправильно'
 //   Перевірте роботу скрипта при test, що дорівнює true, false. Напишіть два варіанти скрипта - з коротким записом(тернаркою)
 //let test = true;
- let test = false;
+let test = false;
 if (test === true) {
     console.log('Неправильно ')
 } else {
@@ -34,8 +34,11 @@ test = test ? "Неправильно" : "Вірно";
 console.log(test);
 
 // 4 Якщо змінна a не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
-let a ;
-a = Math.floor(Math.random() * 3); if (a === 2) {a=-3} // згенеруємо число  -3, 0, 1
+let a;
+a = Math.floor(Math.random() * 3);
+if (a === 2) {
+    a = -3
+} // згенеруємо число  -3, 0, 1
 console.log(a);
 (a !== 0) ? console.log("Вірно") : console.log("Невірно");
 
@@ -48,45 +51,45 @@ dayOfWeeks = parseInt(prompt('Введіть день тижня від 1 до 7
 switch (dayOfWeeks) {
     case 1:
         console.log('ПОНЕДІЛОК: вперед до праці!');
-         break;
+        break;
     case 2:
-         console.log('ВІВТОРОК: вперед до праці!');
-         break;
+        console.log('ВІВТОРОК: вперед до праці!');
+        break;
     case 3:
-         console.log('СЕРЕДА: вперед до праці!');
-         break;
+        console.log('СЕРЕДА: вперед до праці!');
+        break;
     case 4:
-         console.log('ЧЕТВЕР: вперед до праці!');
-         break;
-     case 5:
-         console.log('ПЯТНИЦЯ: вперед до праці!');
-         break;
-     case 6:
-         console.log('СУБОТА: вперед до праці!');
-         break;
+        console.log('ЧЕТВЕР: вперед до праці!');
+        break;
+    case 5:
+        console.log('ПЯТНИЦЯ: вперед до праці!');
+        break;
+    case 6:
+        console.log('СУБОТА: вперед до праці!');
+        break;
     case 7:
-         console.log('НЕДІЛЯ: відпочинок!!!');
-         break;
-     default:
+        console.log('НЕДІЛЯ: відпочинок!!!');
+        break;
+    default:
         console.log("Спробуй ще раз");
- }
+}
 
 
-// Потрібно визначити Високосний рік чи ні. Високосні роки діляться націло на 4.
+// 6. Потрібно визначити Високосний рік чи ні. Високосні роки діляться націло на 4.
+let year;
+year = parseInt(prompt('Введіть рік'));
+if (!!(year)) {
+    if (year % 4 === 0) {
+        console.log(year + ' рік ВИСОКОСНИЙ');
+    } else {
+        console.log(year + ' рік не ВИСОКОСНИЙ');
+    }
+}else {
+    console.log('Спробуй ще раз');
+}
 
-// let year = +prompt('Введіть кількість днів в цьому році');
-//
-// if(year % 2 === 0){
-//     console.log('Рік ВИСОКОСНИЙ');
-// }else {
-//     console.log('УРА! Наш рік нормальний');
-// }
-// ================================================ ТЕРНАРКА + prompt =========================================
-// (+prompt('Введіть поточний рік') % 4 === 0) ? console.log('Рік ВИСОКОСНИЙ') : console.log('УРА! Наш рік нормальний');
-
-
-// - Використовуючи конструкцію if..else, напишіть код, який запитуватиме: „Яка «офіційна» назва JavaScript?“
-// Якщо користувач вводить «ECMAScript», то показати: «Правильно!», інакше – відобразити: «Не знаєте? ECMAScript!
+// Використовуючи конструкцію if..else, напишіть код, який запитуватиме: „Яка «офіційна» назва JavaScript?“<br>
+//     Якщо користувач вводить «ECMAScript», то показати: «Правильно!», інакше – відобразити: «Не знаєте? ECMAScript!<br>
 
 // let oficialName = prompt('Яка «офіційна» назва JavaScript?');
 // if(oficialName === 'ECMAScript'){
