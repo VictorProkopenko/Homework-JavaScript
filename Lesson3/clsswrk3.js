0baaabf8845549d21ee015b577b39b8c1825f9dd
Viktor Prokopenko <victor.prokopenko@gmail.com>
2021-11-16 20:42:37 +0200

    16

--- Lesson3/clsswrk3.js
+++ Lesson3/clsswrk3.js
@@ -4,11 +4,32 @@ while (i < 10) { //перебрати його циклом while
     document.write(`<div><h4> ${array1[i]} </h4></div>`);
     i++;
 }
-for (let n of array1) {
+for (let n of array1) { //перебрати його циклом for
     console.log(n);
 }
 i=1;
-while (i < 10) { //перебрати циклом while та вивести числа тільки з непарним індексом
+while (i < 10) { //4 перебрати циклом while та вивести числа тільки з непарним індексом
+    console.log(i);
     document.write(`<div><h4> ${array1[i]} </h4></div>`);
     i=i+2;
+}
+i=0;
+while (i < 10) { //5 перебрати циклом while та вивести числа тільки парні значення
+    if (array1[i] % 2 === 0) {
+        console.log(i);
+        document.write(`<div><h4> ${array1[i]} </h4></div>`);}
+    i=i+1;
+}
+
+for (i = 0; i<10; i+=1) { //6. перебрати циклом for та вивести числа тільки парні значення
+    if (array1[i] % 2 === 0) {
+        console.log(i);
+        document.write(`<div><h4> ${array1[i]} </h4></div>`);}
+}
+// 7. замінити кожне число кратне 3 на слово "okten"
+for (i = 0; i<10; i++) { //6. перебрати циклом for та вивести числа тільки парні значення
+    if (array1[i] % 3 === 0) {
+        console.log(i);
+        document.write(`<div><h4> ${array1[i]} </h4></div>`);
+        array1[i]="okten";}
 }
