c92a7abcfd407c05162b01d58bd7a869042faa04
viktorpro <victor.prokopenko@gmail.com>
2021-11-17 13:01:07 +0200

    16/11/21

--- Lesson3/clsswrk3.js
+++ Lesson3/clsswrk3.js
@@ -1,35 +1,45 @@
-let array1 = [2,17,13,6,22,31,45,66,100,-18];
+let array1 = [2,17,13,6,22,31,45,66,100,-18]; let i;
 i=1;
+
 while (i < 10) { //перебрати його циклом while
-    document.write(`<div><h4> ${array1[i]} </h4></div>`);
+    document.write(` ${array1[i]} `);
     i++;
 }
+
 for (let n of array1) { //перебрати його циклом for
-    console.log(n);
+    //console.log(n);
+    const p=n+" ";
+    document.write(p);
 }
 i=1;
 while (i < 10) { //4 перебрати циклом while та вивести числа тільки з непарним індексом
-    console.log(i);
-    document.write(`<div><h4> ${array1[i]} </h4></div>`);
-    i=i+2;
+    //console.log(i);
+    document.write(` ${array1[i]} `);
+    i = i + 2; //непарний індекс
 }
 i=0;
 while (i < 10) { //5 перебрати циклом while та вивести числа тільки парні значення
-    if (array1[i] % 2 === 0) {
-        console.log(i);
-        document.write(`<div><h4> ${array1[i]} </h4></div>`);}
+    if (array1[i] % 2 === 0) { //парні значення
+        //console.log(i);
+        document.write(` ${array1[i]} `);}
     i=i+1;
 }
-
 for (i = 0; i<10; i+=1) { //6. перебрати циклом for та вивести числа тільки парні значення
     if (array1[i] % 2 === 0) {
-        console.log(i);
-        document.write(`<div><h4> ${array1[i]} </h4></div>`);}
+        //console.log(i);
+        document.write(` ${array1[i]} `);}
 }
-// 7. замінити кожне число кратне 3 на слово "okten"
-for (i = 0; i<10; i++) { //6. перебрати циклом for та вивести числа тільки парні значення
-    if (array1[i] % 3 === 0) {
-        console.log(i);
-        document.write(`<div><h4> ${array1[i]} </h4></div>`);
+// // 7. замінити кожне число кратне 3 на слово "okten"
+for (i = 0; i<10; i++) {
+    if (array1[i] % 3 === 0) { //замінити кожне число кратне 3 на слово "okten"
+        //console.log(i);
         array1[i]="okten";}
+        document.write(` ${array1[i]} `);
+
+}
+// //8. вивести масив в зворотньому порядку.
+i=10;
+while (i < 1) { //перебрати його циклом while
+    i=i-1;
+    document.write(`<div><h4> ${array1[i]} </h4></div>`);
 }
