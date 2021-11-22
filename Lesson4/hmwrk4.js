6fcc0547142941f5008544afc84cd855d03e5345
Viktor Prokopenko <victor.prokopenko@gmail.com>
2021-11-22 21:34:19 +0200

    22 11 21

--- Lesson4/hmwrk4.js
+++ Lesson4/hmwrk4.js
@@ -25,25 +25,31 @@ function typeArray(array1) {
 }
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 function paragraf(txt) {
-
+    document.write(`<p> ${txt} </p>`);
 }
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 function spisok(sp) {
-    console.log(arguments);
-    return;
+    document.write(`<ul> <li>${sp}</li> <li>${sp}</li> <li>${sp}</li> </ul>`)
 }
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 function spisok2(sp, n) {
-    console.log(arguments);
-    return;
+    document.write(`<ul>`);
+    for (let i = 0; i < n; i++) {
+        document.write(`<li> ${sp} </li>`);
+    }
+    document.write(`</ul>`);
 }
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-function spisokArray(arr, n) {
-    console.log(arguments);
-    return;
+function spisokArray(array1) {
+    document.write(`<ul>`);
+    for (let i = 0; i < array1.length; i++) {
+        document.write(`<li> ${array1[i]} </li>`);
+    }
+    document.write(`</ul>`);
 }
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 function viewArray(arrObj) {
-    console.log(arguments);
-    return;
+    for (let i = 0; i < arrObj.length; i++) {
+        document.write(`<div> ${arrObj.id} </div>`);
+    }
 }
