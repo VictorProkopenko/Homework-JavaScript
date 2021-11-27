6c5cbe7f09711c4004fed39571445c948c15ae69
viktorpro <victor.prokopenko@gmail.com>
2021-11-27 18:00:14 +0200

    25/11/21

--- Lesson6/clswrk6.js
+++ Lesson6/clswrk6.js
@@ -1,21 +1,50 @@
 // - Дано список імен.
-let n1 = 'Harry..Potter'; let n2 = 'Ron---Whisley'; let n3 = 'Hermione__Granger';
+let n1 = 'Harry..Potter';
+let n2 = 'Ron---Whisley';
+let n3 = 'Hermione__Granger';
 console.log(n1, n2, n3);
 document.write(`<div> Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд </div>`);
-// const validName = (n) => {
-// return n;
-// };
-// n1 = validName(n1);
+const validName = (n1, garb) => {
+    n1 = n1.replaceAll(garb, ' ');
+    return n1;
+};
+n1 = validName(n1, '..');
+n2 = validName(n2, '---');
+n3 = validName(n3, '__');
 document.write(`${n1}, ${n2}, ${n3}`);
 document.write(`<div> - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100. </div>`);
+let kol = 10;
+console.log(kol);
+const randomNat = (kol) => {
+    let arr1 = [kol]; let i;
+    for (i = 0; i < kol; i++) {
+        arr1[i] = Math.round(Math.random() * 100);
+    }
+    console.log(arr1);
+    return arr1;
+};
+arr1 = randomNat(kol);
+document.write(`<div> ${arr1} </div>`);
 document.write(`<div> - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort </div>`);
-document.write(`<div> - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, 
+arr1 = randomNat(10);
+arr1 = arr1.sort((a, b) => a - b );
+document.write(`<div> ${arr1} </div>`);
+arr1 = randomNat(10);
+arr1 = arr1.sort((a, b) => b - a);
+document.write(`<div> ${arr1} </div>`);
+
+document.write(`<div> - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових 
+значень. відфільтрувати  його за допомоги filter, 
 залишивши тільки парні числа</div>`);
+arr1 = randomNat(10);
+arr1 = arr1.filter(val => val%2===0);
+document.write(`<div> ${arr1} </div>`);
+
 document.write(`<div>- створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . 
 за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.</div>`);
 document.write(`<div>- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого,
  або навпаки в залежності від значення аргументу direction.</div>`);
-let nums = [11,21,3];
+let nums = [11, 21, 3];
 console.log(nums);
 // sortNums('ascending') // [3,11,21]
 // sortNums('descending') // [21,11,3]
