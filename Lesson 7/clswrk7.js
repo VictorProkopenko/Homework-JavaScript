87a41a9ef9f24a7f36bb9736f99617d0829fc1e8
viktorpro <victor.prokopenko@gmail.com>
2021-12-03 17:20:59 +0200

    01/12/21

--- Lesson 7/clswrk7.js
+++ Lesson 7/clswrk7.js
@@ -13,7 +13,7 @@ function Cars(model,produser,yearCar,maxSpeed,engineCar) {
     this.maxSpeed = maxSpeed;
     this.engineCar = engineCar;
     this.funDrive = function() {
-        console.log(`їдемо зі швидкістю ${maxSpeed} на годину`);
+        console.log(`їдемо зі швидкістю ${maxSpeed} км на годину`);
     };
     this.funInfo = function() { //виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
         console.log(`Модель ${model} `);
@@ -49,7 +49,8 @@ console.log(newCars);
 console.log(`Водій ${newCars.driver} `);
 //
 // - (Те саме, тільки через клас)
-// Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість,
+// об'єм двигуна. додати в об'єкт функції:
 // -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
 // -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
@@ -59,10 +60,74 @@ console.log(`Водій ${newCars.driver} `);
 //
 class Cars2 {
     constructor(model,produser,yearCar,maxSpeed,engineCar) {
+        this.model = model;
+        this.produser = produser;
+        this.yearCar =yearCar;
+        this.maxSpeed = maxSpeed;
+        this.engineCar = engineCar;
+        this.funDrive = function() {
+            console.log(`Їдемо зі швидкістю ${maxSpeed} км на годину`);
+        };
+        this.funInfo = function() { //виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+            for (const key in newCars2) {
+                if (typeof newCars2[key] !== 'function') console.log(`${key}  = ${newCars2[key]}`)
+                        }
+        };
+        this.increaseMaxSpeed = function (newSpeed) {
+            // підвищує значення максимальної швидкості на значення newSpeed
+            this.maxSpeed = this.maxSpeed+newSpeed;
+
+        };
+        this.changeYear = function (newValue) {
+            // змінює рік випуску на значення newValue
+            this.yearCar = newValue;
+        };
+        this.addDriver = function (driver) {
+            // приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+            return this.driver = driver;
+        };
     }
 }
+
+let newCars2 = new Cars2( 'Range Rover', 'Land Rover', 1996, 130, 4.6);
+console.log(newCars2);
+newCars2.funDrive();
+newCars2.funInfo();
+newCars2.increaseMaxSpeed(20);
+newCars2.changeYear(1997);
+newCars2.addDriver('Bill');
+newCars2.funInfo();
+
 //
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
 //     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+class Cinderella { constructor(name, age, fSize) {
+    this.name = name;
+    this.age = age;
+    this.fSize = fSize;}
+}
+class Prince { constructor(name, age, fSize) {
+        this.name = name;
+        this.age = age;
+        this.fSize = fSize;
+    }
+}
+let cinderellas=[];
+for (let i=0;i<10;i++) {
+    cinderellas[i]=new Cinderella('Cind '+i,14+i,31+i)
+}
+console.log(cinderellas);
+let prince = new Prince('Garry', 30, 36);
+for (let i=0; i<cinderellas.length; i++) {
+    //console.log(cinderellas[i].fSize, prince.fSize);
+    if (cinderellas[i].fSize === prince.fSize) {
+        console.log(prince);
+        console.log(`Знайшов  = ${cinderellas[i].name}`);
+        i=cinderellas.length;
+    } else {
+        console.log(`Не знайшов`);
+    }
+}
+console.log(cinderellas.find(sFind => sFind.fSize === prince.fSize));
