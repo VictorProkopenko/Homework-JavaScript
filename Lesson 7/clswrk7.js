67ccc9da995846a1bd7e2e2132a084b7b12c9f98
viktorpro <victor.prokopenko@gmail.com>
2021-12-03 15:14:51 +0200

    01/12/21

--- Lesson 7/clswrk7.js
+++ Lesson 7/clswrk7.js
@@ -12,29 +12,41 @@ function Cars(model,produser,yearCar,maxSpeed,engineCar) {
     this.yearCar =yearCar;
     this.maxSpeed = maxSpeed;
     this.engineCar = engineCar;
-    this.funDrive = function funDrive() {
+    this.funDrive = function() {
         console.log(`їдемо зі швидкістю ${maxSpeed} на годину`);
     };
-    this.funInfo = function funInfo() { //виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+    this.funInfo = function() { //виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
         console.log(`Модель ${model} `);
         console.log(`Виробник ${produser} `);
         console.log(`Рік випуску ${yearCar} `);
         console.log(`Максимальна швидкість ${maxSpeed} `);
         console.log(`Об'єм двигуна ${engineCar} `);
     };
-    this.increaseMaxSpeed = function increaseMaxSpeed (newSpeed) {
+    this.increaseMaxSpeed = function (newSpeed) {
         // підвищує значення максимальної швидкості на значення newSpeed
+        this.maxSpeed = this.maxSpeed+newSpeed;
+
     };
-    this.changeYear = function changeYear (newValue) {
+    this.changeYear = function (newValue) {
         // змінює рік випуску на значення newValue
+        this.yearCar = newValue;
     };
-    this.addDriver = function addDriver (driver) {
+    this.addDriver = function (driver) {
         // приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+        return this.driver = driver;
     };
 }
-let newCars = [];
-newCars[0] = new Cars('Ford Mustang', 'Ford', '2004','180 км/год','3.8 л');
+let newCars = new Cars('Ford Mustang', 'Ford', 2004,180,'3.8 л');
 console.log(newCars);
+newCars.funDrive();
+newCars.funInfo();
+newCars.increaseMaxSpeed(15);
+newCars.changeYear(2005);
+//newCars.driver=newCars.addDriver('Jon');
+newCars.addDriver('Jon');
+//newCars.driver='Jon';
+console.log(newCars);
+console.log(`Водій ${newCars.driver} `);
 //
 // - (Те саме, тільки через клас)
 // Створити клас який дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
@@ -45,6 +57,10 @@ console.log(newCars);
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
 //
 //
+class Cars2 {
+    constructor(model,produser,yearCar,maxSpeed,engineCar) {
+    }
+}
 //
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
