747911492ec3ce125df19fdfcd5bd39e877502a2
viktorpro <victor.prokopenko@gmail.com>
2021-12-09 14:03:57 +0200

    06/12/21

--- Lesson 10/clswrk10.js
+++ Lesson 10/clswrk10.js
@@ -13,15 +13,44 @@ document.getElementById('btn1').onclick = function () {
 //     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
 // (Додаткова частина для завдання)
 //
-//
+let table1 = document.forms.form3;
+function buildTable () {
+    let r = +table1.row.value;
+    let c = +table1.col.value;
+    let cont = table1.cont.value;
+    console.log(r,c,cont);
+    let table = document.createElement('table');
+    table.style.border = '1px solid black';
+    for (let i=0; i < r; i++) {
+        let trow = document.createElement("tr");
+        for (let j=0; j < c; j++) {
+            let tcol = document.createElement("td");
+            trow.appendChild(tcol);
+            tcol.innerText = cont;
+        }
+        table.appendChild(trow);
+    }
+    document.body.appendChild(table);
+};
+
 // - Сворити масив не цензцрних слів.
+let notCenz = ['test1','test2','test3'];
 //     Сворити інпут текстового типу.
 //     Якщо людина вводить слово і воно міститься в масиві не цензурних слів
 // кинути алерт з попередженням.
 //     Перевірку робити при натисканні на кнопку
 //
+function testCenz2(n) {
+    let wrdb = document.forms[n].cenz.value;
+    if (notCenz.includes(wrdb)) {
+        alert('Not good '+wrdb);
+    } else {
+        alert('Good!');
+    }
+}
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Потрібно перевіряти чи не містить ціле речення в собі погані слова.
 //     Кинути алерт з попередженням у випадку якщо містить.
 //     Перевірку робити при натисканні на кнопку
+
