a4bbd81f448ce87f862d31e684bb47983c691042
viktorpro <victor.prokopenko@gmail.com>
2021-12-16 17:24:16 +0200

    15/12/21 fetch

--- Lesson 12/clswrk12.js
+++ Lesson 12/clswrk12.js
@@ -2,3 +2,28 @@
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/posts
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(posts => posts.json())
+    .then(postsArray => {
+        let classPosts = document.getElementsByClassName('posts')[0];
+        for (let postsArrayElement of postsArray) {
+            let divPostTitle = document.createElement('h5');
+            let divPostBody = document.createElement('div');
+            let divPostId = document.createElement('div');
+            let divPostBtn = document.createElement('button');
+            divPostTitle.innerText = `Title: ${postsArrayElement.title}`;
+            divPostBody.innerHTML = `<span style="font-style: italic ; background:yellow;"> ${postsArrayElement.body}</span>`;
+            divPostId.innerText = `Id: ${postsArrayElement.id} UserId: ${postsArrayElement.userId}`;
+            divPostBtn.innerText = `Коментарі`;
+            classPosts.append(divPostId,divPostTitle,divPostBody,divPostBtn);
+            divPostBtn.onclick = function () {
+                fetch(`https://jsonplaceholder.typicode.com/posts/${postsArrayElement.id}/comments`)
+                    .then(comment => comment.json())
+                    .then(commentArray => {
+                        console.log(commentArray);
+                    });
+            };
+        }
+
+
+    });
