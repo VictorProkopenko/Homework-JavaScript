3cb767d6467df7d684e9b8b87354b810d1143890
viktorpro <victor.prokopenko@gmail.com>
2021-12-22 17:53:11 +0200

    21 12 21 mySQL

--- Lesson 14/hmwrk14.js
+++ Lesson 14/hmwrk14.js
@@ -1,10 +1,38 @@
 //зробити від 6 до 9 кроків розпорядок дня - встати почистіті зуби поїсти
 //зробити через колбеки проміс авейт
 const money = 1000;
-let btnCallback = document.getElementById('btn1');
+let btnCallback  = document.getElementById('btn1');
 let btnCallback2 = document.getElementById('btn2');
 let btnCallback3 = document.getElementById('btn3');
 let btnCallback4 = document.getElementById('btn4');
+
+const pidyom = new Day(); // функції підйом
+const gigiena = new Day(); // функції гігієна
+const eat = new Day(); // функції їжа
+const way = new Day(); // функції дорога
+const work = new Day(); // функції робота
+const beer = new Day(); // функції пиво
+const sleep = new Day(); // функції сон
+
+btnCallback.onclick = function () {step1(null);
+    console.log('This is the start Callback');
+    console.log('Callback виявився геть не страшним');
+    function step1(error) {if (error) { console.log('Error step 1')}
+        else {pidyom.cB2(money, `1. Підйом`, 50, 1500,() =>step2)} }
+    function step2(error) {if (error) { console.log('Error step 2')}
+        else {gigiena.cB2(money, `2. Гігієна`, 100, 100,() =>step3)} }
+    function step3(error) {if (error) { console.log('Error step 3')}
+        else {eat.cB2(money, `3. їжа`, 250, 150,() => step4)} }
+    function step4(error) {if (error) { console.log('Error step 4')}
+        else {way.cB2(money, `4. дорога вперед`, 7, 1500,() =>step5)} }
+    function step5(error) {if (error) { console.log('Error step 5')}
+        else {way.cB2(money, `5. робота`, -1000, 4000,() =>step6)} }
+    function step6(error) {if (error) { console.log('Error step 6')}
+        else {console.log('Step 6')} }
+}
+
+
+
 // btnCallback.onclick = function () {
 //     console.log('This is the start Callback');
 //     step1(null);
@@ -28,6 +56,8 @@ let btnCallback4 = document.getElementById('btn4');
 //     // {cb(money, `8. сон`, 0, 4000,() =>
 //     // {cb(money, `End`, 0, 0, () => console.log('callback'));});});});});});});});});
 // };
+
+
 btnCallback2.onclick = function (resolve, reject) {
     console.log('This is the start Promice');
     cb2(money, `1. Підйом`, 50, 1500)
@@ -62,14 +92,14 @@ btnCallback3.onclick = function () {
 }
 btnCallback4.onclick = function () {
     console.log('Тестовий приклад без синхронізації');
-  cb2(money, `1. Підйом`, 50, 500);
-  cb2(money, `2. Гігієна`, 100, 100);
-  cb2(money, `3. їжа`, 250, 150);
-  cb2(money, `4. дорога вперед`, 7, 1500);
-  cb2(money, `5. робота`, -1000, 4000);
-  cb2(money, `6. пиво`, 100, 1000);
-  cb2(money, `7. дорога`, 7, 1300);
-  cb2(money, `8. сон`, 0, 3000);
+    pidyom.cB(money, `1. Підйом`, 50, 1500);
+    gigiena.cB(money, `2. Гігієна`, 100, 100);
+    eat.cB(money, `3. їжа`, 250, 150);
+    way.cB(money, `4. дорога вперед`, 7, 1500);
+    work.cB(money, `5. робота`, -1000, 4000);
+    beer.cB(money, `6. пиво`, 100, 1000);
+    way.cB(money, `7. дорога`, 7, 1300);
+    sleep.cB(money, `8. сон`, 0, 3000);
 }
 
     function cb(money, message, kredit, timeout, callback) {
@@ -91,3 +121,22 @@ btnCallback4.onclick = function () {
             }, timeout)
         });
     }
+
+// Создаём функцию, принимающую два аргумента, и возвращающую их сумму
+
+
+function Day(money, message, kredit, timeout) {
+    this.cB = function(money, message, kredit, timeout) {
+        setTimeout(() => {
+            //resolve(money);
+            console.log(`${message} ${money} Timeout:${timeout}`);
+            return money -= kredit;
+        }, timeout)};
+    this.cB2 = function(money, message, kredit, timeout, callback){
+        setTimeout(() => {
+            callback();
+            console.log(`${message} ${money} Time:${timeout}`);
+            return money -= kredit;;
+        }, timeout);
+    }
+}
