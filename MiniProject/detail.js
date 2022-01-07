bf56fd1bf2a00a091d67eae0d3dd360f6ba9fd9f
Viktor Prokopenko <victor.prokopenko@gmail.com>
2022-01-07 23:34:01 +0200

    21 12 21

--- MiniProject/detail.js
+++ MiniProject/detail.js
@@ -5,9 +5,9 @@ const content = document.createElement('div');
 const addressElement = document.createElement('div');
 const companyElement = document.createElement('div');
 const postBtn = document.createElement('button');
-
 let address = users.address;
 let company = users.company;
+
 postBtn.innerText = 'post of current user';
 content.innerHTML = `Id: ${users.id} name:${users.name} username:${users.username} email:${users.email} 
 phone:${users.phone} website:${users.website}`;
@@ -15,15 +15,18 @@ addressElement.innerHTML = ` Address street: ${address.street} suite: ${address.
 geo: lat | lng ${address.geo.lat} : ${address.geo.lng} `;
 companyElement.innerHTML = ` Company Name: ${company.name} , Catch: ${company.catchPhrase} , Bs: ${company.bs} `;
 postBtn.onclick = () => {
-    console.log(users.id);
     fetch('https://jsonplaceholder.typicode.com/users/' + users.id + '/posts')
         .then(response => {
            return response.json();
         })
         .then(posts => {
-            for (let post of posts) {
+            for (const post of posts) {
                 const userPosts = document.createElement('div');
-                userPosts.innerHTML = `<h4>Title: ${post.title}</h4>`;
+                userPosts.innerHTML = `<a href='post-details.html'> <h5> ${post.title} </h5> </a>`;
+                userPosts.addEventListener('click', userPost => {
+                    let posts=`post`;
+                    localStorage.setItem(posts, JSON.stringify(post));
+                });
                 container.appendChild(userPosts);
                 postBtn.disabled = true;
             }
@@ -34,3 +37,7 @@ content.appendChild(companyElement);
 content.appendChild(postBtn);
 userDiv.appendChild(content);
 container.appendChild(userDiv);
+
+
+
+
